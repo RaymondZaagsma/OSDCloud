c5b57a4d5054cf21437e802baafc0b3c91257481
RaymondZaagsma <65401355+RaymondZaagsma@users.noreply.github.com>
2022-01-13 07:52:11 +0100

    Initial commit

---
+++ Archive/Readme.ps1
@@ -0,0 +1,6 @@
+#If ScriptPad is displaying this script, then you need to update your OSD PowerShell Module
+#================================================
+#   [PreOS] Install and Import OSD Module
+#================================================
+Install-Module OSD -Force
+Import-Module OSD -Force
